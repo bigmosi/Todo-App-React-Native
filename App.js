23e66ee55c17b3246102a7d2b2dcf8fc745e6ee8
bigmosi <kinyeramo@gmail.com>
2021-11-14 16:04:24 +0300

    setting up the project

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet} from 'react-native';
+
+
+const COLORS = {primary: '#1f145c', white: '#fff'};
+
+const App =() => {
+
+  return (
+    <SafeAreaView>
+       <View>
+         <Text>Todo App</Text>
+       </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
