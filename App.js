7c0156a40fda5069bc9e34d050a69011a270dd0f
bigmosi <kinyeramo@gmail.com>
2021-11-14 17:30:09 +0300

    adding flatlist and vector icon

--- App.js
+++ App.js
@@ -1,5 +1,14 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet} from 'react-native';
+import { 
+  SafeAreaView, 
+  StyleSheet, 
+  View, 
+  Text, 
+  TextInput, 
+  TouchableOpacity,
+  FlatList 
+} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 
 const COLORS = {primary: '#1f145c', white: '#fff'};
@@ -7,15 +16,63 @@ const COLORS = {primary: '#1f145c', white: '#fff'};
 const App =() => {
 
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{flex: 1, backgroundColor: COLORS.white}}>
+       <View style={styles.header}>
+         <Text style={{fontWeight: 'bold', fontSize: 20, color: COLORS.primary}}>TODO APP</Text>
+         <Icon name="delete"  size={25} color="red" />
+       </View>
+       <FlatList />
+       <View style={styles.footer}>
+         <View style={styles.inputContainer}>
+           <TextInput placeholder="Add Todo" />
+         </View>
+         <TouchableOpacity>
+           <View style={styles.iconContainer}>
+             <Icon name="add" size={30} color={COLORS.white}/>
+           </View>
+         </TouchableOpacity>
+       </View>
        <View>
-         <Text>Todo App</Text>
        </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  header: {
+    padding: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  footer: {
+    position: 'absolute',
+    bottom: 0,
+    color: COLORS.white,
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  inputContainer: {
+    backgroundColor: COLORS.white,
+    elevation: 40,
+    flex: 1,
+    height: 50,
+    marginVertical: 20,
+    marginRight: 20,
+    borderRadius: 20,
+    paddingHorizontal: 20
+  },
+  iconContainer: {
+    height: 50,
+    width: 50,
+    backgroundColor: COLORS.primary,
+    borderRadius: 25,
+    elevation: 40,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
   
 });
 
